bd8b0618c525f9484db3d761ef8d2420db7fb87d
omar hussain <omarhussain23moh@gmail.com>
2023-02-09 17:15:30 -0400

    create-react-app installation

---
+++ cheeze-jokes/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+      </header>
+      <main>
+      </main>
+    </div>
+  );
+}
+
+export default App;
